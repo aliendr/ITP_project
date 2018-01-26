e8ec2c1419383016cd02ff8e311f64680cf37313
SyomaKiss <s.kiselev@innopolis.ru>
2018-01-26 12:18:49 +0300

    2nd edition not working in Ilia pc

--- lybrary/src/main/java/templater/PageGenerator.java
+++ lybrary/src/main/java/templater/PageGenerator.java
@@ -11,7 +11,7 @@ import java.io.Writer;
 import java.util.Map;
 
 public class PageGenerator {
-    private static final String HTML_DIR = "templates";
+    private static final String HTML_DIR = "web";
 
     private static PageGenerator pageGenerator;
     private final Configuration cfg;
